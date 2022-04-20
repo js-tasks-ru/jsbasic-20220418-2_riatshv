bd74864008ef7b3b98781dabbb136617ef935f81
Riat Sakhapov <sakhapov.riat@gmail.com>
2022-04-20 14:11:14 +0300

    Completed factorial function

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (parseInt(n) || parseInt(n) == 0) {
+    let fact = 1;
+    for (let i = 2; i <= n; i++) {
+      fact*=i;
+    }
+    return fact;
+  } else {
+    return "Вы ввели не числовое значение"
+  }
 }
