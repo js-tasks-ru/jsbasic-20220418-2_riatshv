d57cfc7dbaaa7c272f30d39331d3aaf174c61056
Riat Sakhapov <sakhapov.riat@gmail.com>
2022-04-20 16:19:07 +0300

    Completed spam function

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,16 @@
 function checkSpam(str) {
-  // ваш код...
+  let spamWords = ["1xbet", "xxx"];
+  let strLowerCase = str.toLowerCase();
+	
+  let isInclude = false;
+  let i = 0;
+  
+	while (!isInclude && i < spamWords.length) {
+    if (strLowerCase.includes(spamWords[i])) {
+      isInclude = true;
+    }
+    i++;
+  }
+  
+  return isInclude;
 }
