2d0e23c3a6cf45c4f00861542abe31cbf2cd0879
Riat Sakhapov <sakhapov.riat@gmail.com>
2022-04-28 17:23:14 +0300

    Completed filter users function

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = "";
+	users.filter(item => item.age <= age)
+    .forEach((item, ndx, array) => {
+    if (ndx == array.length - 1) return  str += `${item.name}, ` + `${item.balance}`;
+    
+    return  str += `${item.name}, ` + `${item.balance}\n`
+  });
+  
+  return str;
 }
