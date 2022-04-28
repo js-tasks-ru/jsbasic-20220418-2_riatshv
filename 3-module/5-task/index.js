2d24ccf60b60444f8a2a4cf4f29b33210d60d7d1
Riatshv <102540349+Riatshv@users.noreply.github.com>
2022-04-28 17:59:41 +0300

    Update index.js

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,6 +1,6 @@
 function getMinMax(str) {
   let result = {};
-  let arrNumbers = str.split(" ").filter(item => parseInt(item) || parseInt(item) == 0;
+  let arrNumbers = str.split(" ").filter(item => parseInt(item) || parseInt(item) == 0);
   
   result.min = Math.min(...arrNumbers)
   result.max = Math.max(...arrNumbers)
