38aa24082ddee1b19239be60602c44302aac1cac
Riat Sakhapov <sakhapov.riat@gmail.com>
2022-04-28 17:53:38 +0300

    Completed find min and max from string function

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {};
+  let arrNumbers = str.split(" ").filter(item => parseInt(item));
+  
+  result.min = Math.min(...arrNumbers)
+  result.max = Math.max(...arrNumbers)
+
+
+  return result;
 }
