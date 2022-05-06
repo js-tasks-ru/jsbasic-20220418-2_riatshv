5962f0285045f96ff278b5737060c851805b0da0
Riat Sakhapov <sakhapov.riat@gmail.com>
2022-05-07 00:40:35 +0300

    Completed highlight users function

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+
+  for (let row of table.rows) {
+
+    let lastRow = row.lastElementChild
+    console.log(lastRow);
+    if ( lastRow.dataset.available == "true" ) {
+      row.classList.add("available");
+    } else if ( lastRow.dataset.available == "false" ) {
+      row.classList.add("unavailable");
+    } else {
+      row.hidden = true;
+    }
+
+
+    let gender = lastRow.previousElementSibling;
+    if ( gender.textContent == "m" ) {
+      row.classList.add("male");
+    } else if ( gender.textContent == "f" ) {
+      row.classList.add("female");
+    } 
+
+    let age = gender.previousElementSibling;
+    if ( parseInt(age.textContent) < 18 ) {
+      row.style.textDecoration = "line-through"
+    }
+  }
 }
