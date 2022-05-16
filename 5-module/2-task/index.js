10dc1e812ea17151dfce6c5333b796f6a9f410e3
Riat Sakhapov <sakhapov.riat@gmail.com>
2022-05-16 18:07:10 +0300

    Completed hide-text function

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector(".toggle-text-button");
+  const text = document.querySelector("#text");
+  let count = 0;
+
+  button.onclick = ( (event) => {
+    if (count % 2 == 0) {
+      text.hidden = true;
+    } else {
+      text.hidden = false;
+    }
+    count++;
+  });
 }
