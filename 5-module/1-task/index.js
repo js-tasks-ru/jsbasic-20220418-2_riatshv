eb89277de587ccc9d36e8dba84dc4c99f04c4223
Riat Sakhapov <sakhapov.riat@gmail.com>
2022-05-16 17:56:08 +0300

    Completed hide-button function

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,8 @@
 function hideSelf() {
-  // ваш код...
+  const button = document.querySelector(".hide-self-button");
+
+  button.onclick = (() => {
+    button.hidden = true;
+    button.onclick = null;
+  })
 }
