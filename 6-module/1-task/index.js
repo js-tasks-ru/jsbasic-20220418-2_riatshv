4b3edb4073ba2e42514d8212e4900cee16598f10
Riat Sakhapov <sakhapov.riat@gmail.com>
2022-05-19 18:50:53 +0300

    Completed array-to-table function

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,45 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+
+    const elem = `
+      <thead>
+          <tr>
+              <th>Имя</th>
+              <th>Возраст</th>
+              <th>Зарплата</th>
+              <th>Город</th>
+              <th></th>
+          </tr>
+      </thead>
+      <tbody>
+        ${this.rows.map( (item) => {
+          let temp = `<tr>`
+          for (let key in item) {
+            temp += `
+            <td>${item[key]}</td>`
+          }
+          temp += `<td> <button>X</button> </td>`
+          return temp + `</tr>`;
+        } ).join(" ")}
+      </tbody>
+    `;
+    
+    const parent = document.createElement("table");
+    parent.innerHTML = elem;
+
+    this.elem = parent; 
+
+    const tbody = this.elem.querySelector("tbody");
+
+    
+    tbody.querySelectorAll("tr").forEach( (row) => {
+      const removeBtn = row.querySelector("button");
+
+      removeBtn.addEventListener("click", () => {
+        row.remove();
+      });
+    })
   }
 }
